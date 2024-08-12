1f58e1936fc075f5de8cb72365dec4b0bb572bea
Mat <mail@mathias.is>
2024-08-12 21:07:27 +0300

    Add missing copyright headers to files

--- src/soulsetup.d
+++ src/soulsetup.d
@@ -1,12 +1,12 @@
 /+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  + SoulFind - Free SoulSeek server                                           +
  +                                                                           +
- + Copyright(C) 2005 SeeSchloss <seeschloss@seeschloss.org>                 +
+ + Copyright (C) 2005 SeeSchloss <seeschloss@seeschloss.org>                 +
  +                                                                           +
  + This  program  is free software ; you can  redistribute it  and/or modify +
  + it under  the  terms of  the GNU General Public License  as published  by +
  + the  Free  Software  Foundation ;  either  version  2 of  the License, or +
- +(at your option) any later version.                                       +
+ + (at your option) any later version.                                       +
  +                                                                           +
  + This  program  is  distributed  in the  hope  that  it  will  be  useful, +
  + but   WITHOUT  ANY  WARRANTY ;  without  even  the  implied  warranty  of +
