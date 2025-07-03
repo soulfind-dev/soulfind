soulfind-dev/soulfind

commit 2e2cb573115df9c11fa4ca58c18914777af4ae88

setup.d: add command to register new users
It doesn't check name restrictions, but since those are checked on
login anyway, we probably don't have to.

src/setup/setup.d
@@ -279,14 +279,33 @@ class Setup
         show_menu(
             format!("Registered users (%d)")(db.num_users),
             [
-                MenuItem("1", "Show user info",        &user_info),
-                MenuItem("2", "Change user password",  &change_password),
-                MenuItem("3", "Remove user",           &remove_user),
-                MenuItem("4", "List registered users", &list_registered),
-                MenuItem("5", "List privileged users", &list_privileged),
+                MenuItem("1", "Add user",              &add_user),
+                MenuItem("2", "Show user info",        &user_info),
+                MenuItem("3", "Change user password",  &change_user_password),
+                MenuItem("4", "Remove user",           &remove_user),
+                MenuItem("5", "List registered users", &list_registered),
+                MenuItem("6", "List privileged users", &list_privileged),
                 MenuItem("q", "Return",                &main_menu)
             ]
         );
+    }
+
+    private void add_user()
+    {
+        write("Username : ");
+        const username = input.strip;
+
+        if (db.user_exists(username)) {
+            writefln!("\nUser %s is already registered")(username);
+            registered_users();
+            return;
+        }
+
+        write("Password : ");
+        const password = input.strip;
+
+        db.add_user(username, password);
+        registered_users();
     }
 
     private void user_info()
@@ -340,7 +359,7 @@ class Setup
         registered_users();
     }
 
-    private void change_password()
+    private void change_user_password()
     {
         write("User to change password of : ");
         const username = input.strip;

commit dc82cc26038dcf03fdac032a6ae943c17ed26736

db.d: make functions take actual password instead of hash
This is in preparation for proper password hashing beyond insecure
MD5 hashes.

Also fixes a regression in 2e2cb573115df9c11fa4ca58c18914777af4ae88
where the plain password was passed when adding users.

src/db.d
@@ -11,6 +11,8 @@ import soulfind.defines : blue, default_max_users, default_port, norm;
 import std.array : Appender;
 import std.conv : ConvException, to;
 import std.datetime : Clock, SysTime;
+import std.digest : digest, LetterCase, secureEqual, toHexString;
+import std.digest.md : MD5;
 import std.file : exists, isFile;
 import std.stdio : writefln, writeln;
 import std.string : format, fromStringz, join, replace, toStringz;
@@ -270,13 +272,20 @@ class Sdb
         return query(sql, [username]).length > 0;
     }
 
+    private string hash_password(string password)
+    {
+        return digest!MD5(password).toHexString!(LetterCase.lower).to!string;
+    }
+
     void add_user(string username, string password)
     {
         const sql = format!(
             "INSERT INTO %s(username, password) VALUES(?, ?);")(
             users_table
         );
-        query(sql, [username, password]);
+        const hash = hash_password(password);
+
+        query(sql, [username, hash]);
         query("PRAGMA optimize;");
 
         debug(user) writefln!("Added new user %s")(blue ~ username ~ norm);
@@ -290,6 +299,33 @@ class Sdb
         query(sql, [username]);
 
         debug(user) writefln!("Removed user %s")(blue ~ username ~ norm);
+    }
+
+    bool user_verify_password(string username, string password)
+    {
+        const sql = format!(
+            "SELECT password FROM %s WHERE username = ?;")(
+            users_table
+        );
+        const stored_hash = query(sql, [username])[0][0];
+        const current_hash = hash_password(password);
+
+        return secureEqual(current_hash, stored_hash);
+    }
+
+    void user_update_password(string username, string password)
+    {
+        const sql = format!(
+            "UPDATE %s SET password = ? WHERE username = ?;")(
+            users_table
+        );
+        const hash = hash_password(password);
+
+        query(sql, [hash, username]);
+
+        debug(user) writefln!("Set user %s's password")(
+            blue ~ username ~ norm
+        );
     }
 
     bool user_exists(string username)
@@ -444,28 +480,6 @@ class Sdb
             try banned_until = res[0][0].to!long; catch (ConvException) {}
 
         return SysTime.fromUnixTime(banned_until);
-    }
-
-    string get_user_password(string username)
-    {
-        const sql = format!(
-            "SELECT password FROM %s WHERE username = ?;")(
-            users_table
-        );
-        return query(sql, [username])[0][0];
-    }
-
-    void set_user_password(string username, string password)
-    {
-        const sql = format!(
-            "UPDATE %s SET password = ? WHERE username = ?;")(
-            users_table
-        );
-        query(sql, [password, username]);
-
-        debug(user) writefln!("Set user %s's password")(
-            blue ~ username ~ norm
-        );
     }
 
     SdbUserStats user_stats(string username)

src/server/messages.d
@@ -698,7 +698,7 @@ class SMessage
 class SLogin : SMessage
 {
     this(bool success, string message, uint ip_address = 0,
-         string password = null, bool supporter = false) scope
+         string md5_hash = null, bool supporter = false) scope
     {
         super(Login);
 
@@ -707,7 +707,7 @@ class SLogin : SMessage
 
         if (success) {
             write!uint(ip_address);
-            write!string(password);
+            write!string(md5_hash);
             write!bool(supporter);
         }
     }

src/server/user.d
@@ -127,11 +127,6 @@ class User
         return true;
     }
 
-    private string encode_password(string password)
-    {
-        return digest!MD5(password).toHexString!(LetterCase.lower).to!string;
-    }
-
     private string verify_login(string username, string password)
     {
         ulong max_users;
@@ -147,16 +142,14 @@ class User
             return "INVALIDUSERNAME";
 
         if (!server.db.user_exists(username)) {
-            server.db.add_user(username, encode_password(password));
+            server.db.add_user(username, password);
             return null;
         }
         debug (user) writefln!("User %s is registered")(
             blue ~ username ~ norm
         );
 
-        if (!secureEqual(
-                server.db.get_user_password(username),
-                encode_password(password)))
+        if (!server.db.user_verify_password(username, password))
             return "INVALIDPASS";
 
         return null;
@@ -634,9 +627,11 @@ class User
                 server.add_user(this);
                 watch(username);
 
+                const md5_hash = digest!MD5(msg.password)
+                    .toHexString!(LetterCase.lower)
+                    .to!string;
                 scope response_msg = new SLogin(
-                    true, motd, address.addr, encode_password(msg.password),
-                    supporter
+                    true, motd, address.addr, md5_hash, supporter
                 );
                 scope room_list_msg = new SRoomList(server.room_stats);
                 scope wish_interval_msg = new SWishlistInterval(
@@ -1053,9 +1048,7 @@ class User
             case ChangePassword:
                 scope msg = new UChangePassword(msg_buf, username);
 
-                server.db.set_user_password(
-                    username, encode_password(msg.password)
-                );
+                server.db.user_update_password(username, msg.password);
 
                 scope response_msg = new SChangePassword(msg.password);
                 send_message(response_msg);

src/setup/setup.d
@@ -366,11 +366,9 @@ class Setup
 
         if (db.user_exists(username)) {
             write("Enter new password : ");
-            const password = digest!MD5(input)
-                .toHexString!(LetterCase.lower)
-                .to!string;
+            const password = input;
 
-            db.set_user_password(username, password);
+            db.user_update_password(username, password);
         }
         else
             writefln!("\nUser %s is not registered")(username);